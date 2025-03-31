f3de2fe997215cd52083e838c09e48b9c70af522
ThanksAbout <sasharulit902@gmail.com>
2025-03-31 19:30:56 +0200

    Functions_update

--- thank_you.php
+++ thank_you.php
@@ -7,22 +7,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Thank You for Booking!</title>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-        
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        
-        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700&display=swap" rel="stylesheet">
-
-        <link href="css/bootstrap.min.css" rel="stylesheet">
-
-        <link href="css/bootstrap-icons.css" rel="stylesheet">
-
-        <link href="css/owl.carousel.min.css" rel="stylesheet">
-
-        <link href="css/owl.theme.default.min.css" rel="stylesheet">
-
-        <link href="css/templatemo-medic-care.css" rel="stylesheet">
-   
 </head>
 <body>
     <h2>Thank You for Your Booking!</h2>
